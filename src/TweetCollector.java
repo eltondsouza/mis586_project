a75387d293f769902ee4a81047f4e208de0866e5
elton-d <eltoncdsouza@gmail.com>
2014-09-28 13:08:17 -0700

    Added more friend JSONs

---
+++ src/TweetCollector.java
@@ -0,0 +1,45 @@
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterResponse;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
+import twitter4j.conf.ConfigurationBuilder;
+
+import java.awt.*;
+import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Properties;
+public class TweetCollector {
+
+	
+	public static void main(String[] args) throws TwitterException {
+	    
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+    	cb.setDebugEnabled(true)
+    	  .setOAuthConsumerKey("dstu8zdadkx2hf4CHpcegKTNy")
+    	  .setOAuthConsumerSecret("64cCY6jjsdJFI0nwD5zfddSjfFgD3AqD3vBOSdHBw3T7ZAB11Z")
+    	  .setOAuthAccessToken("2777354556-9QpSxTFiLRq4HXTpMrlQ06sCM0vxr7K5qyDSVY7")
+    	  .setOAuthAccessTokenSecret("qvQhJBXdQEdavKgvNxnJyq5EnQYZk1SDNmSnMSjSkkJa1")
+    	  .setJSONStoreEnabled(true);
+    	TwitterFactory tf = new TwitterFactory(cb.build());
+    	Twitter twitter = tf.getInstance();
+    	
+    	Query query = new Query("#bitcoin #cryptocurrency");
+    	    QueryResult result = twitter.search(query);
+    	    
+    	    int counter = 0;
+    	    
+    	    for (Status status : result.getTweets()) {
+    	        System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
+    	        
+    	        if(counter++ == 10)
+    	        	System.exit(0);
+    	    }
+	}
+}
