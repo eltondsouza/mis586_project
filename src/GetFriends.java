4274637e8dad996898a08c4899dcde8a89c8f274
elton-d <eltoncdsouza@gmail.com>
2014-10-16 20:38:34 -0700

    connected to MongoDB

---
+++ src/GetFriends.java
@@ -0,0 +1,210 @@
+//Twitter4j imports
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+import twitter4j.IDs;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.TwitterObjectFactory;
+import twitter4j.User;
+import twitter4j.conf.ConfigurationBuilder;
+import com.mongodb.BasicDBList;
+//Mongo imports
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+public class GetFriends {
+
+	public static void main(String a[]) throws UnknownHostException, NumberFormatException
+	{
+		MongoClient mongoClient = new MongoClient( "localhost" );
+		DB db = mongoClient.getDB( "bigdata" );
+		DBCollection coll = db.getCollection("cryptoUsers");
+		DBCollection friendsList = db.getCollection("friendsList");
+		
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+    	cb.setDebugEnabled(true)
+    	  .setOAuthConsumerKey("5at9oIC6aqWOGYE635Cw8dX8z")
+    	  .setOAuthConsumerSecret("ACvypDRJWO5Ni2HDQxYvm9Z4uSD3MZhxVou92gFJvLg1MOO2wG")
+    	  .setOAuthAccessToken("39952747-IcSqsSoPphTWaLoIwYaUITWQ5PmeyV40bQHgkTf4C")
+    	  .setOAuthAccessTokenSecret("Nn4NEH9XE9xIb6QXu3R8kOdocncplz67Cq8RZlrOZlyOu")
+    	  .setJSONStoreEnabled(true);
+    	TwitterFactory tf = new TwitterFactory(cb.build());
+    	Twitter twitter = tf.getInstance();
+		
+		DBCursor mongo_cursor = coll.find();
+		int counter = 0;
+		
+		try {
+			   while(mongo_cursor.hasNext()) 
+			   {
+			      
+			       
+				    String id = (String)mongo_cursor.next().get("id_str");
+				    String screen_name = (String)mongo_cursor.curr().get("screen_name");
+				    System.out.println(id+":"+screen_name);
+					//System.out.println(counter);
+					   
+					boolean exists = true;
+					User currUser = null;		   
+					//check if user exists
+					try {
+						currUser = twitter.showUser(Long.parseLong(id));
+					} 
+					   
+					catch (TwitterException te) {
+					
+						if(te.getStatusCode()==404)
+	            		{
+	            			exists = false;
+	            			System.out.println("Could not find user"+screen_name);
+	            			
+	            		}
+	
+	            		else if(te.getStatusCode()==429)
+	            		{
+	            			System.out.println("Waiting...");
+	            			try {
+								Thread.sleep(15*60*1000);
+							} 
+	            			
+	            			catch (InterruptedException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+	            			
+	            			try {
+	            				currUser = twitter.showUser(Long.parseLong(id));
+							} 
+	            			
+	            			catch (TwitterException e) {
+								if(te.getStatusCode()==404)
+			            		{
+			            			exists = false;
+			            			System.out.println("Could not find user"+screen_name);
+			            			
+			            		}
+							}
+	            		}
+						
+					}
+					   
+					   if(exists==false)
+						   continue;
+						   
+					   IDs FriendsIDs = null;
+					   long twitterCursor = -1;
+					   
+					   do{
+						
+						   boolean noFriends = false;
+						   try 
+						    {
+							    //get upto 5000 friends of currUser
+								FriendsIDs = twitter.getFriendsIDs(Long.parseLong(id),twitterCursor,5000);
+							} 
+						
+						   catch (TwitterException e) 
+						   {
+							   if(e.getStatusCode()==429)
+							   {
+								   System.out.println("Waiting...");
+			            			try {
+										Thread.sleep(15*60*1000);
+										FriendsIDs = twitter.getFriendsIDs(Long.parseLong(id),twitterCursor,5000);
+									} 
+			            			
+			            			catch (InterruptedException f) 
+			            			{
+										// TODO Auto-generated catch block
+										f.printStackTrace();
+									}
+			            			catch(TwitterException g)
+			            			{
+			            				System.out.println("Could not find friends for "+screen_name);
+			            				noFriends = true;
+			            			}
+							   }
+							   
+							   else
+							   {
+								   System.out.println("Could not find friends for "+screen_name);
+								   noFriends = true;
+							   }
+						   }
+						   
+						   if(noFriends==true)
+							   continue;
+						   
+						   //get Friends IDs as an array of long
+						   long[] FriendsIDsLong = FriendsIDs.getIDs();
+						   
+						   //create DBObject				   
+						   BasicDBObject doc = new BasicDBObject("id", Long.parseLong(id))
+					        .append("screen_name", screen_name)
+					        .append("friends", FriendsIDsLong);
+						   
+						   //insert the DBObject
+						   friendsList.insert(doc);
+					   }   
+					   while ((twitterCursor = FriendsIDs.getNextCursor()) != 0);
+					   System.out.println("User no: "+counter++);
+				    
+			   }
+			} finally {
+			   mongo_cursor.close();
+			}
+		
+	}
+	private static void storeJSON(String rawJSON, String fileName) throws IOException {
+        FileOutputStream fos = null;
+        OutputStreamWriter osw = null;
+        BufferedWriter bw = null;
+        try {
+            fos = new FileOutputStream(fileName);
+            osw = new OutputStreamWriter(fos, "UTF-8");
+            bw = new BufferedWriter(osw);
+            bw.write(rawJSON);
+            bw.flush();
+        } finally {
+            if (bw != null) {
+                try {
+                    bw.close();
+                } catch (IOException ignore) {
+                }
+            }
+            if (osw != null) {
+                try {
+                    osw.close();
+                } catch (IOException ignore) {
+                }
+            }
+            if (fos != null) {
+                try {
+                    fos.close();	
+                } catch (IOException ignore) {
+                }
+            }
+        }
+    }
+
+}
